bf2db6bd4e2cb8dd2bf85a56896a81f38ba172d6
abdullah10182 <abdullah10182@gmail.com>
2021-08-25 07:50:07 -0400

    init commit

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CritterApplication {
 
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(CritterApplication.class, args);
 	}
