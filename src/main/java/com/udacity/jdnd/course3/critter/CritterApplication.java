d9587424d90a50bffbf62f910d87a0b400a1d5b9
abdullah10182 <abdullah10182@gmail.com>
2021-08-26 09:03:52 -0400

    added h2 database

--- src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
+++ src/main/java/com/udacity/jdnd/course3/critter/CritterApplication.java
@@ -2,16 +2,26 @@ package com.udacity.jdnd.course3.critter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Launches the Spring application. Unmodified from starter code.
  */
+@RestController
 @SpringBootApplication
 public class CritterApplication {
 
 	//test
 	public static void main(String[] args) {
 		SpringApplication.run(CritterApplication.class, args);
+	}
+
+	@GetMapping
+	public String home() {
+		return "Ehloo";
 	}
 
 }
